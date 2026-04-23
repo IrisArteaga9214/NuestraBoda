803786b910b727e18a10542891dc00e89deaa529
IrisArteaga9214 <irisarteaga9214@gmail.com>
2026-04-22 18:26:04 -0600

    Delete components directory

--- components/wedding/gallery.tsx
+++
@@ -1,4 +0,0 @@
-'use client'
-import Image from 'next/image'
-import { GallerySection } from './gallery-section'
-export { GallerySection as Gallery }
--- components/wedding/hero.tsx
+++
@@ -1,2 +0,0 @@
-'use client'
-export { Hero } from './hero-section'
